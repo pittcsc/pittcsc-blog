cb4c8a2f75ffff9321797b8864379f25c35132b0
GameDog9988 <alex@grattan.me>
2021-12-19 09:01:59 -0500

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addWatchTarget("./src/sass/");
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/js");
+  // eleventyConfig.addPassthroughCopy("./src/img");
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
+};
