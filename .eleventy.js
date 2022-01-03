29db5d8b0d777b7a19229eb0710de6efb5415ae6
GameDog9988 <alex@grattan.me>
2022-01-03 15:53:33 -0500

    feat: favicon

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/js");
   eleventyConfig.addPassthroughCopy("./src/images");
+  eleventyConfig.addPassthroughCopy("./src/apple-touch-icon.png");
+  eleventyConfig.addPassthroughCopy("./src/favicon-16x16.png");
+  eleventyConfig.addPassthroughCopy("./src/favicon-32x32.png");
   return {
     dir: {
       input: "src",
