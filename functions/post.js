e5bfa2b7abb43d365e0589300d79cfee60014001
GameDog9988 <alex@grattan.me>
2022-01-14 18:23:43 -0500

    feat: create get all netlify function

--- functions/post.js
+++ functions/post.js
@@ -4,14 +4,17 @@
 const formattedReturn = require("./helpers/formattedReturn");
 const dislikePost = require("./helpers/dislikePost");
 const likePost = require("./helpers/likePost");
+const getPosts = require("./helpers/getPosts");
 
 exports.handler = async (event) => {
-  const body = JSON.parse(event.body);
-  // console.log(body);
-  if (body.like == "increment") {
-    return await likePost(event);
-  } else if (body.like == "decrement") {
-    return await dislikePost(event);
+  if (event.httpMethod === "GET") {
+    return await getPosts(event);
+  } else if (event.httpMethod === "POST") {
+    if (JSON.parse(event.body).like == "increment") {
+      return await likePost(event);
+    } else if (JSON.parse(event.body).like == "decrement") {
+      return await dislikePost(event);
+    }
   } else {
     return formattedReturn(405, {});
   }
