09b65a79727143a70c2696675b7dcd3bdd444692
GameDog9988 <alex@grattan.me>
2022-01-24 22:31:41 -0500

    fix: comment out adding of branches manually

--- .eleventy.js
+++ .eleventy.js
@@ -21,14 +21,14 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addCollection("tagList", function (collection) {
     let tagSet = new Set();
-    tagSet.add("game dev");
-    tagSet.add("panther ml");
-    tagSet.add("pitt cyber security");
-    tagSet.add("robotics");
-    tagSet.add("algo trading");
-    tagSet.add("swift dev");
-    tagSet.add("math in cs");
-    tagSet.add("rust dev");
+    // tagSet.add("game dev");
+    // tagSet.add("panther ml");
+    // tagSet.add("pitt cyber security");
+    // tagSet.add("robotics");
+    // tagSet.add("algo trading");
+    // tagSet.add("swift dev");
+    // tagSet.add("math in cs");
+    // tagSet.add("rust dev");
     collection.getAll().forEach((item) => {
       if (item.data.tags != undefined) {
         (item.data.tags || []).forEach((tag) => tagSet.add(tag));
@@ -38,7 +38,7 @@ module.exports = function (eleventyConfig) {
     return tagSet;
   });
 
-  eleventyConfig.addShortcode('excerpt', article => extractExcerpt(article));
+  eleventyConfig.addShortcode("excerpt", (article) => extractExcerpt(article));
 
   return {
     dir: {
@@ -49,28 +49,32 @@ module.exports = function (eleventyConfig) {
 };
 
 function extractExcerpt(article) {
-	if (!article.hasOwnProperty('templateContent')) {
-	  console.warn('Failed to extract excerpt: Document has no property "templateContent".');
-	  return null;
-	}
-   
-	let excerpt = null;
-	const content = article.templateContent;
-
-	// The start and end separators to try and match to extract the excerpt
-	const separatorsList = [
-	  { start: '<!-- Excerpt Start -->', end: '<!-- Excerpt End -->' },
-	  { start: '<p>', end: '</p>' }
-	];
-   
-	separatorsList.some(separators => {
-	  const startPosition = content.indexOf(separators.start);
-	  const endPosition = content.indexOf(separators.end);
-   
-	  if (startPosition !== -1 && endPosition !== -1) {
-		excerpt = content.substring(startPosition + separators.start.length, endPosition).trim();
-		return true; // Exit out of array loop on first match
-	  }
-	});
-	return excerpt;
+  if (!article.hasOwnProperty("templateContent")) {
+    console.warn(
+      'Failed to extract excerpt: Document has no property "templateContent".'
+    );
+    return null;
   }
+
+  let excerpt = null;
+  const content = article.templateContent;
+
+  // The start and end separators to try and match to extract the excerpt
+  const separatorsList = [
+    { start: "<!-- Excerpt Start -->", end: "<!-- Excerpt End -->" },
+    { start: "<p>", end: "</p>" },
+  ];
+
+  separatorsList.some((separators) => {
+    const startPosition = content.indexOf(separators.start);
+    const endPosition = content.indexOf(separators.end);
+
+    if (startPosition !== -1 && endPosition !== -1) {
+      excerpt = content
+        .substring(startPosition + separators.start.length, endPosition)
+        .trim();
+      return true; // Exit out of array loop on first match
+    }
+  });
+  return excerpt;
+}
