788c0bfc4a525cd40c973d3d5175620ad616dcac
Nathan Barta <71276691+NathanBarta@users.noreply.github.com>
2023-08-31 21:49:40 -0400

    Change names

--- src/_data/seo.js
+++ src/_data/seo.js
@@ -1,8 +1,8 @@
 module.exports = {
   baseURL: "https://pittcsc-blog.netlify.app/",
-  title: "Pitt Computer Science Club Blog",
+  title: "Computer Science Club at Pitt Blog",
   description:
     "Blog posts and articles written by members of the computer science club at the University of Pittsburgh.",
-  twitter: "pittcsc",
+  twitter: "CSC_at_Pitt",
   image: "/images/Pitt_CSC_Blog_OG_Image.png",
 };
